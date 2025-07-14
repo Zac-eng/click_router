c963b8eaaf4d13899f288703346e70c1b978a75f
Zac-eng <zack.abten@gmail.com>
2025-07-14 15:55:37 +0900

    modified mytcp reorder

--- localelement/mytcpreorder.cc
+++ localelement/mytcpreorder.cc
@@ -1,147 +1,80 @@
-/* 
- * MyTCPReorder.{cc,hh} -- reorders TCP packets based on sequence numbers
- */
-
 #include "mytcpreorder.hh"
+CLICK_DECLS
 
-MyTCPReorder::MyTCPReorder() 
-    : _capacity(1000), _timeout_ms(1000), _next_seq(0),
-      _timer(timer_hook, this), _buffer_count(0),
-      _total_packets(0), _reordered_packets(0) {
+#define TABLE_CAP 20
+
+MyTCPReorder::MyTCPReorder()
+: _next_id(0)
+{
+  _packet_map = new HashTable<uint16_t, Packet *>;
 }
 
 MyTCPReorder::~MyTCPReorder() {
-    // Clean up any packets still in buffer
-    for (PacketMap::iterator it = _packet_map.begin(); 
-         it != _packet_map.end(); ++it) {
+    for (HashTable <uint32_t, Packet *>::iterator it = _packet_map.begin(); 
+        it != _packet_map.end(); ++it) {
         it->second->kill();
     }
+    delete _packet_map;
 }
 
-int MyTCPReorder::configure(Vector<String> &conf, ErrorHandler *errh) {
-    if (cp_va_kparse(conf, this, errh,
-                    "CAPACITY", cpkP+cpkM, cpUnsigned, &_capacity,
-                    "TIMEOUT", cpkP, cpUnsigned, &_timeout_ms,
-                    cpEnd) < 0)
-        return -1;
-    
-    _timer.initialize(this);
-    if (_timeout_ms > 0)
-        _timer.schedule_after_msec(_timeout_ms);
-    
-    return 0;
+int
+MyTCPReorder::configure(Vector<String> & conf, ErrorHandler *errh)
+{
+  return 0;
 }
 
-void MyTCPReorder::push(int, Packet *p) {
-    // Extract TCP header and sequence number
-    const click_ip *iph = p->ip_header();
+void
+MyTCPReorder::flush_packets()
+{
+  while (true) {
+    Packet* next_packet = _packet_map->get(_next_id);
+    if (!next_packet) {
+      break;
+    }
+    output(0).push(next_packet);
+    _packet_map->erase(_next_id);
+    ++_next_id;
+  }
+}
+
+void
+MyTCPReorder::push(int port, Packet *p)
+{
+    click_ip     *iph = (click_ip *) (p -> ip_header());
     if (!iph || iph->ip_p != IP_PROTO_TCP) {
         // Not a TCP packet, pass it through
-        printf("non tcp\n");
-        output(1).push(p);
+        output(0).push(p);
         return;
     }
-
     const click_tcp *tcph = p->tcp_header();
     if (!tcph) {
-        output(1).push(p);
+        output(0).push(p);
         return;
     }
-
-    _total_packets++;
     uint32_t seq = ntohl(tcph->th_seq);
-    printf("%d\n", seq);
-    // Initialize next_seq if this is the first packet
-    if (_packet_map.empty() && _next_seq == 0) {
-        _next_seq = seq;
-    }
-
-    // If this is the next expected packet, send it immediately
-    if (seq == _next_seq) {
-        uint32_t len = p->length() - (p->transport_header_offset() + (tcph->th_off << 2));
-        _next_seq += len > 0 ? len : 1;  // Move to next expected sequence
+    if (seq == _next_id) {
+        click_chatter("in sequence: %d\n", seq);
         output(0).push(p);
-        
-        // Check if we can release more packets
-        check_release();
-        return;
+        ++_next_id;
+        flush_packets();
     }
-
-    // Buffer is full, drop oldest packet
-    if (_buffer_count >= _capacity) {
-        Packet *old_p = _packet_map.begin()->second;
-        _packet_map.erase(_packet_map.begin());
-        _buffer_count--;
-        old_p->kill();
+    else if (seq < _next_id) {
+        click_chatter("out-of-order, killed: %d\n", seq);
+        p->kill();
     }
-
-    // Store the packet for later
-    _packet_map.insert(std::make_pair(seq, p));
-    _buffer_count++;
-    _reordered_packets++;
-
-    // Reschedule timer if needed
-    if (_timeout_ms > 0 && !_timer.scheduled())
-        _timer.schedule_after_msec(_timeout_ms);
-}
-
-void MyTCPReorder::check_release() {
-    release_packets();
-}
-
-void MyTCPReorder::release_packets() {
-    // Release any packets that are now in order
-    while (!_packet_map.empty()) {
-        PacketMap::iterator it = _packet_map.begin();
-        if (it->first != _next_seq)
-            break;
-        
-        Packet *p = it->second;
-        _packet_map.erase(it);
-        _buffer_count--;
-
-        const click_tcp *tcph = p->tcp_header();
-        uint32_t len = p->length() - (p->transport_header_offset() + (tcph->th_off << 2));
-        _next_seq += len > 0 ? len : 1;
-        
-        output(0).push(p);
+    else {
+        click_chatter("out-of-order: %d, size: %d\n", seq, _packet_map->size());
+        (*_packet_map)[seq] = p->clone();
+        p->kill();
+        if (_packet_map->size() > TABLE_CAP) {
+            while (!_packet_map->get(_next_id)) {
+                ++_next_id;
+            }
+            flush_packets();
+        }
     }
 }
 
-void MyTCPReorder::timer_hook(Timer *t, void *user_data) {
-    MyTCPReorder *e = static_cast<MyTCPReorder*>(user_data);
-    
-    // If we've waited too long, flush everything
-    if (!e->_packet_map.empty()) {
-        // Update next_seq to the oldest packet's sequence
-        e->_next_seq = e->_packet_map.begin()->first;
-        e->release_packets();
-    }
-    
-    // Reschedule if there are still packets
-    if (!e->_packet_map.empty() && e->_timeout_ms > 0)
-        t->schedule_after_msec(e->_timeout_ms);
-}
-
-String MyTCPReorder::read_handler(Element *e, void *thunk) {
-    MyTCPReorder *r = static_cast<MyTCPReorder*>(e);
-    switch (reinterpret_cast<intptr_t>(thunk)) {
-    case 0:
-        return String(r->_buffer_count);
-    case 1:
-        return String(r->_total_packets);
-    case 2:
-        return String(r->_reordered_packets);
-    default:
-        return String();
-    }
-}
-
-void MyTCPReorder::add_handlers() {
-    add_read_handler("buffersize", read_handler, (void*)0);
-    add_read_handler("total", read_handler, (void*)1);
-    add_read_handler("reordered", read_handler, (void*)2);
-}
-
+CLICK_ENDDECLS
 EXPORT_ELEMENT(MyTCPReorder)
 ELEMENT_MT_SAFE(MyTCPReorder)
--- localelement/mytcpreorder.hh
+++ localelement/mytcpreorder.hh
@@ -1,45 +1,39 @@
-#include <click/config.h>
+#ifndef MyTCP_REORDER_HH
+#define MyTCP_REORDER_HH
 #include <click/element.hh>
-#include <click/confparse.hh>
-#include <click/error.hh>
-#include <click/packet.hh>
-#include <clicknet/tcp.h>
+#include <click/hashtable.hh>
+#include <click/ipaddress.hh>
+#include <click/config.h>
+#include <click/args.hh>
+#include <click/glue.hh>
 #include <clicknet/ip.h>
-#include <click/timer.hh>
-#include <map>
+#include <clicknet/udp.h>
+#include <clicknet/tcp.h>
+#include <clicknet/icmp.h>
+#include <click/ipaddress.hh>
+#include <click/straccum.hh>
+
+CLICK_DECLS
 
 class MyTCPReorder : public Element {
 public:
-    MyTCPReorder();
-    ~MyTCPReorder();
+  MyTCPReorder() CLICK_COLD;
+  ~MyTCPReorder() CLICK_COLD;
 
-    const char *class_name() const { return "MyTCPReorder"; }
-    const char *port_count() const { return "1-/2"; }
-    const char *processing() const { return PUSH; }
+  const char *class_name() const override	{ return "MyTCPReorder"; }
+  const char *port_count() const override	{ return "1-/1"; }
+  const char *processing() const override	{ return PUSH; }
 
-    int configure(Vector<String> &, ErrorHandler *);
-    void push(int port, Packet *p);
-
-    static String read_handler(Element *e, void *thunk);
-    void add_handlers();
+  int configure(Vector<String> &conf, ErrorHandler *errh) CLICK_COLD;
+  void push(int, Packet *p);
+  // void add_handlers() CLICK_COLD;
 
 private:
-    // Parameters
-    uint32_t _capacity;
-    uint32_t _timeout_ms;
+  HashTable <uint32_t, Packet *> *_packet_map;
+  uint32_t _next_id;
 
-    // Data structures for packet reordering
-    typedef std::multimap<uint32_t, Packet*> PacketMap;
-    PacketMap _packet_map;
-    uint32_t _next_seq;
-    Timer _timer;
-
-    // State tracking
-    uint32_t _buffer_count;
-    uint32_t _total_packets;
-    uint32_t _reordered_packets;
-
-    void check_release();
-    void release_packets();
-    static void timer_hook(Timer*, void*);
+  void flush_packets();
 };
+
+CLICK_ENDDECLS
+#endif
